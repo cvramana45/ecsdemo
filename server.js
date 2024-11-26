4f8e07854f87c8f844c42a572b5c6025c68708e6
cvramana45 <116644172+cvramana45@users.noreply.github.com>
2024-11-26 17:34:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 'use strict';
-
+ 
 const express = require('express');
-
+const path = require('path');
 // Constants
  const PORT = 80;
 const HOST = '0.0.0.0';
  // App
  const app = express();
+ app.use(express.static(path.join(__dirname)));
+ 
  app.get('/', (req, res) => {
-   res.send(`<h1>Welcome to RAMANA triabz AS A PM</h1>`);
+   res.send(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Welcome</title>
+    </head>
+    <body>
+        <h1>wELCOME TO TRIANZ RAMANA CV AS A PM</h1>
+        <img src="cv.png" alt="Sample Image" width="500"/>
+    </body>
+    </html>
+    `);
    });
-
+ 
    app.listen(PORT, HOST);
    console.log(`Running on http://${HOST}:${PORT}`);
+ 
+ 
