8714e6ea6effe6424142d46c26b593f0ae1544cb
cvramana45 <116644172+cvramana45@users.noreply.github.com>
2024-11-26 17:04:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ const HOST = '0.0.0.0';
  // App
  const app = express();
  app.get('/', (req, res) => {
-   res.send('<h1>Welcome to RAMANA CV AS A PM</h1>
-            <img src="/cv.png" alt="Sample Image" width="500"/>');
+   res.send(`<h1>Welcome to RAMANA CV AS A PM</h1>
+            <img src="cv.png" alt="Sample Image" width="500"/>
+    `);
    });
 
    app.listen(PORT, HOST);
